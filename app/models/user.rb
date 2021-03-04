2d896cc34f67d5f5cc664dcac1b745af2f138658
abeyuyajp <yellowmonkey0618@gmail.com>
2021-03-04 11:56:33 +0900

    バリデーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nickname
+    validates :password_confirmation, format: {with: /[a-z\d]{6,}/i}
+    validates :team
+    validates :position
+  end
+    validates :password, format: {with: /[a-z\d]{6,}/i}
 end
