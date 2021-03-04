9bb8560433e3072360f724c08274270a0df2f75a
abeyuyajp <yellowmonkey0618@gmail.com>
2021-03-04 16:32:48 +0900

    Factorybotの導入

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :user do
-    
+    nickname              {'test'}
+    email                 {'test@example'}
+    password              {'test0000'}
+    password_confirmation {password}
+    team                  {'テスト'}
+    position              {'player'}
   end
 end
