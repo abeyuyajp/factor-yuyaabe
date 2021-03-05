754a8414c94ac81ecd59d9e5aa6e04506a598fcd
abeyuyajp <yellowmonkey0618@gmail.com>
2021-03-05 19:00:53 +0900

    投稿機能の単体テスト

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   belongs_to :user
 
   with_options presence: true do
-    validates :title
+    validates :title, length: { maximum: 100, message: "が長すぎます" }
     validates :youtube_url
     validates :text
   end
