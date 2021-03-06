ba6dda9047f483c5ef261b0642f6b4c4bb83d5fb
abeyuyajp <yellowmonkey0618@gmail.com>
2021-03-06 22:50:53 +0900

    投稿一覧機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
 
   def index
+    @posts = Post.includes(:user).order("created_at DESC")
   end
 
   def new
@@ -9,6 +11,10 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
+    url = params[:post][:youtube_url]
+    url = url.last(11)
+    @post.youtube_url = url
+
     if @post.save
       redirect_to root_path
     else
