115d25fc4353dc624211baf01a37832090614345
abeyuyajp <yellowmonkey0618@gmail.com>
2021-03-10 16:56:20 +0900

    マイページ機能の実装

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -38,17 +38,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  protected
-
-    # 更新時にパスワード入力を省く
-    def update_resource(resource, params)
-      resource.update_without_password(params)
-    end
-
-    # 更新後のパスを指定
-    def after_update_path_for(resource)
-      user_path(@user.id)
-    end
+  #protected
   
 
   # If you have extra params to permit, append them to the sanitizer.
