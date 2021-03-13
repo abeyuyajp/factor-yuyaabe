feab97f3ea2776d2f91d0cea001f5965bc188146
abeyuyajp <yellowmonkey0618@gmail.com>
2021-03-13 11:03:01 +0900

    投稿時刻の日本時間への修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,9 +1,10 @@
 class MessagesController < ApplicationController
+  before_action :authenticate_user! 
 
   def create
     if RoomUser.where(user_id: current_user.id, room_id: params[:message][:room_id]).present?
       @message = Message.create(message_params)
-      redirect_to rooms_path(@message.room_id)
+      redirect_to room_path(@message.room_id)
     else
       flash[:alert] = "メッセージの送信に失敗しました。"
     end
