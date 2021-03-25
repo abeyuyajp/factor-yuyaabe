3def3d19005d947ee1a2ce21bcd2a76973df52f8
abeyuyajp <yellowmonkey0618@gmail.com>
2021-03-25 15:49:51 +0900

    deploy.rbの修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,30 +1,23 @@
-# capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
-lock 'capistrano (3.16.0)'
 
-# Capistranoのログの表示に利用する
+lock '3.16.0'
+
 set :application, 'factor-yuyaabe'
 
-# どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:abeyuyajp/factor-yuyaabe.git'
 
-# バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.6.5' #カリキュラム通りに進めた場合、’2.6.5’ です
+set :rbenv_ruby, '2.6.5' 
 
-# どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
                                   keys: ['~/.ssh/abeyuya.pem'] 
 
-# プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 
-# Unicornの設定ファイルの場所
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
-# デプロイ処理が終わった後、Unicornを再起動するための記述
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
